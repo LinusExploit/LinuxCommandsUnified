819d95142a60a1dad9806d8abd8110023626c72e
linusExploit <moh.hsj@gmail.com>
2021-05-24 14:14:21 +0400

    Update finder.py

--- finder.py
+++ finder.py
@@ -5,7 +5,7 @@ import sys
 f = open('LinuxCommandsUnified.text','r')
 all = f.read()
 
-search =  '{{ {} }}.*?{{.*?}}'.format(sys.argv[1])
+search =  '[ {} ].*?[.*?]$'.format(sys.argv[1])
 result =  re.findall(search, all, re.DOTALL)
 if result:
     print(result[0])
